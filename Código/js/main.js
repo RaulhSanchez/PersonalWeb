069082effe923eda73bcaffab8eecf44ebc0858a
Raul <raaul9212@gmail.com>
2021-10-14 08:59:28 +0200

    players

--- Código/js/main.js
+++ Código/js/main.js
@@ -1,1 +1,30 @@
 
+let player1 = {
+  nombre : "Tu imaginacion",
+  vida:100,
+  ataqueImagincacion()
+}
+
+let player2 = {
+  nombre : "Tu imaginacion",
+  vida=100,
+  ataqueImagincacion()
+}
+
+let player3 ={
+  nombre: "Tu imaginacion",
+  vida=100,
+  ataqueImagincacion()
+}
+
+let player4 = {
+  nombre: "Tu imaginacion",
+  vida=100,
+  ataqueImagincacion()
+}
+
+//function click(){
+  //  document.getElementsByClassName("botonJuego")
+//}
+
+const ataqueImagincacion = ataque =>({});
