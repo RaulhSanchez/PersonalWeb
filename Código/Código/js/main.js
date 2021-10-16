31ac96b8a847be644e5d30c43eb823786be542bf
Raul <raaul9212@gmail.com>
2021-10-16 19:41:55 +0200

    Pantalla Seleccion CSS

--- Código/Código/js/main.js
+++ Código/Código/js/main.js
@@ -1,3 +1,5 @@
+let mainScreen =document.getElementById("seleccion").style.visibility="visible";
+let gameScreen = document.getElementById("game").style.visibility="visible";
 
 class player {
   constructor(name,damage, life, playerImage){
@@ -8,77 +10,71 @@ class player {
   }
 };
 
-let player00 = new player('j1',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
-let player01 = new player('j1',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
-let player02 = new player('j1',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
-let player03 = new player('j1',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
+let player00 = new player('player00',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
+let player01 = new player('player01',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
+let player02 = new player('player02',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
+let player03 = new player('player03',-5, 100, "https://i.pinimg.com/550x/3a/00/00/3a00006d51658920c717c28fbdd7b789.jpg")
 
-let game ={
-  players:[]
+
+
+let game={
+  player:[]
 }
 
+console.log(game)
 
 
-function playerSelecter(playerSeleced){
 
-  if(game.players.length == 2){
 
-    // Añado las imagenes de los jugadores seleccionados en la pantalla de pelea(oculta)
-    if(onclick){
-      let playerImage = game.players[0].image;
-    }
+function playerSelecter(playerSelected){
+  
+
+  if(game.player.length == 2){
+    console.log(playerSelected);
+    let mainScreen =document.getElementById("seleccion").style.visibility;
+    mainScreen =document.getElementById("seleccion").style.visibility="hiden";
     
-    console.log(onclick); 
-    // Cambio de pantall, desocilto la pantalla de la pelea que previamente he rellenado con los jugadores seleccionados
-    let nodoMainScreen = documen.getElementById("seleccion")
-    nodoMainScreen.style.display='none'
 
-    let nodoPantallaPelea = document.getElementById("pelea")
-    nodoMainScreen.style.display = 'block'
+    let gameScreen = document.getElementById("game");
+    gameScreen.style.visibility="visible";
 
-  }
-  else{
-    if(game.players[0] == undefined){
+   }
+   else{
 
-      if(playerSeleced == player00){
-        game.players[0]=player00
-
-      }
-      if(playerSeleced == player01){
-        game.players[0]=player01
-
-      }
-
-      if(playerSeleced == player02){
-        game.players[0]=player02
-
-      }
-      if(playerSeleced == player03){
-        game.players[0]=player03
-
-      }
+    if(game.player[0] == undefined){
+      if(playerSelected == 'player00'){
+          game.player[0] = player00
+      }   
+      if(playerSelected == 'player01'){
+          game.player[0] = player01
+      }   
+      if(playerSelected == 'player02'){
+          game.player[0] = player02
+      }   
+      if(playerSelected == 'player03'){
+          game.player[0] = player03
+      }   
 
     }else{
-      if(playerSeleced == player00){
-        game.players[1]=player00
 
-      }
-      if(playerSeleced == player01){
-        game.players[1]=player01
-
-      }
-
-      if(playerSeleced == player02){
-        game.players[1]=player02
-
-      }
-      if(playerSeleced == player03){
-        game.players[1]=player03
-
+      if(game.player[1] == undefined){
+        if(playerSelected == 'player00'){
+            game.player[1] = player00
+        }   
+        if(playerSelected == 'player01'){
+            game.player[1] = player01
+        }   
+        if(playerSelected == 'player02'){
+            game.player[1] = player02
+        }   
+        if(playerSelected == 'player03'){
+            game.player[1] = player03
+        }   
       }
     }
 
-  }
+    
+   }
 
 
 }
