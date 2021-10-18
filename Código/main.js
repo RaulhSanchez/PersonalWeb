50ec79d2a210b529fae87c6f0b8b87aafe5bfbae
Raul <raaul9212@gmail.com>
2021-10-18 22:43:17 +0200

    Final

---
+++ Código/main.js
@@ -0,0 +1,30 @@
+let valor = localStorage.getItem('LinkedIn');
+alert("Aceptar cookies");
+
+
+let coockies ={
+    nombre:"Aceptadas"
+    }
+    //Almacenamos utilizando la conversión a texto el objeto JSON
+    localStorage.setItem('Datos', JSON.stringify(coockies));
+    //Recuperamos realizando la conversión de texto a Objeto JSON.
+    let resultado = JSON.parse(localStorage.getItem('Datos'));
+    alert('Resultado ->' + coockies.nombre + ' Tipo -> ' +
+    typeof(coockies));
+
+//Eliminar un elemento determinado por su clave.
+localStorage.removeItem('Datos');
+//Para eliminar todo el localStorage o el sessionStorage.
+localStorage.clear();
+
+
+
+
+function handleStorage(event) {
+    event = event || window.event;
+    if (event.newValue === null) { // it was removed
+    // Do somthing
+    } else {
+    // Do somthing else
+    }
+    }
